09d0a3d07aea02594929d3cf9d77797eabf28eb1
Daksh <54631466+daksh931@users.noreply.github.com>
2021-12-25 21:30:22 +0530

    Add files via upload

---
+++ func_over_old.py
@@ -0,0 +1,15 @@
+class info:
+    def dak(self,name=None):
+        if name is not None:
+            print("Welcome dear",name)
+
+        elif name is None:
+            print("cannot greet")
+
+
+d1 = info()
+
+d1.dak()
+
+
+

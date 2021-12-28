06df8e88136e7ce2883634cda4c57689b3d14143
Daksh <54631466+daksh931@users.noreply.github.com>
2021-12-28 21:41:00 +0530

    Add files via upload

---
+++ Display_Largest_number.py
@@ -0,0 +1,16 @@
+a = int(input ('Enter First number - '))
+b = int(input ('Enter Second number - '))
+c = int(input ('Enter Third number - '))
+
+if a>b and a>c:
+  print(a)
+        
+
+elif b>a and b>c:
+  print(b)
+  
+
+else:
+    print(c)
+
+

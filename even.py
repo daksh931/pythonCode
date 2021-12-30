408d902763ce2bbfd0925c84f65a573cde08264e
Daksh <54631466+daksh931@users.noreply.github.com>
2021-12-30 15:24:25 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,11 @@
+a = int(input ('Enter a number - '))
+
+if a==1:
+    print('Entered number is odd')
+
+elif a%2 == 0:
+    print('Entered number is Even')
+    
+else :
+    print('Entered Number is Odd')
+    

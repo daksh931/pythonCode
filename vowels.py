a59ef119cf6555650630face09a49f91becaa6b2
Daksh <54631466+daksh931@users.noreply.github.com>
2022-01-05 22:08:11 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,7 @@
+ab = (input ('Enter an Alphabet - '))
+
+if ab=='a' or ab=='e' or ab=='i' or ab=='o' or ab=='u' :
+    print('You have entered a Vowel ')
+else :
+    print('You have entered a Constant ')
+    

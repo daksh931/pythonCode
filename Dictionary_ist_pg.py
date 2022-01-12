ccfeae59b8aa1ba553e5bdd6276d64b85d8b9ace
Daksh <54631466+daksh931@users.noreply.github.com>
2022-01-12 09:08:44 +0530

    Add files via upload

---
+++ Dictionary_ist_pg.py
@@ -0,0 +1,14 @@
+person = {"Name":"Daksh","Age":20,45:"daksh",True:"deepak"}
+
+
+person["Age"] = 30
+person[45] = "DakshGoyal"
+
+
+
+
+print(person)
+print(person["Name"])
+print(person["Age"])
+print(person[45])
+print(person[True])
